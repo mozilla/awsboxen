02ab1dffcf7598836c79e3be4ab81093b91f95f5
Ryan Kelly <ryan@rfk.id.au>
2013-04-10 15:20:58 +1000

    Implement basic config file loading and merging.

---
+++ test/config.js
@@ -0,0 +1,52 @@
+
+const assert = require('assert');
+const path = require('path');
+
+const config = require('../lib/config');
+
+
+function testDataPath(name) {
+  return path.join(__dirname, 'config-test-data', name);
+}
+
+
+describe('config loader', function() {
+
+  it('can load a simple JSON file', function(done) {
+    config.loadConfig(testDataPath('simple.json'), function(err, res) {
+      assert.equal(err, null);
+      assert.deepEqual(res, {'one': 1, 'map': { 'key': 'value' }});
+      done();
+    });
+  });
+
+  it('can load a simple YAML file', function(done) {
+    config.loadConfig(testDataPath('simple.yml'), function(err, res) {
+      assert.equal(err, null);
+      assert.deepEqual(res, { 'items': ['one', 'two', 'three'] });
+      done();
+    });
+  });
+
+  it('can merge a complex directory structure', function(done) {
+    config.loadConfig(testDataPath('merged'), function(err, res) {
+      assert.equal(err, null);
+      assert.deepEqual(res, {
+        Sub: {
+          extras: ['check', 'check'],
+          Sub2: {
+            one: 1,
+            two: 2,
+            three: 3
+          },
+        },
+        Items: {
+          milk: true,
+          bread: false
+        }
+      });
+      done();
+    });
+  });
+
+});
