f03c539b9d3ef2148efdff7c7f0d369852040ee1
Ryan Kelly <ryan@rfk.id.au>
2013-04-16 10:56:19 +1000

    Add MPL license headers, cleanup some comments.

--- test/config.js
+++ test/config.js
@@ -1,3 +1,12 @@
+/* This Source Code Form is subject to the terms of the Mozilla Public
+ * License, v. 2.0. If a copy of the MPL was not distributed with this
+ * file, You can obtain one at http://mozilla.org/MPL/2.0/. */
+
+/* 
+ * Testcases for configuration file loading.
+ * These reqiure the data files located in ./config-test-data/
+ *
+ */
 
 const assert = require('assert');
 const path = require('path');
