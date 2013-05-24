982c5f41c8b4c5de6c6ba256c5544b71eb8f14a7
Ryan Kelly <rkelly@mozilla.com>
2013-05-24 18:03:37 +1000

    Tweak how profile overrides are processed, and add testcase for it.
    These tweaks make it easier for profiles to override part of the default
    .awsbox.json configuration while leaving other parts of intact.

---
+++ test/projinfo.js
@@ -0,0 +1,79 @@
+/* This Source Code Form is subject to the terms of the Mozilla Public
+ * License, v. 2.0. If a copy of the MPL was not distributed with this
+ * file, You can obtain one at http://mozilla.org/MPL/2.0/. */
+
+/* 
+ * Testcases for projinfo config file loading/merging.
+ * These require the awsboxen projet itself to provide the test data,
+ * so they must be run from a git checkout.
+ *
+ */
+
+const assert = require('assert');
+const path = require('path');
+
+const projinfo = require('../lib/projinfo');
+
+
+var PROJDIR = path.resolve(__dirname, '..');
+console.log(PROJDIR);
+
+var TOP_LEVEL_KEYS = ["AWSTemplateFormatVersion", "Boxen", "Description", 
+                      "Outputs", "Parameters", "Resources"];
+
+describe('projinfo loader', function() {
+
+  it('loads a .awsbox.json file into a default config', function(done) {
+    projinfo(PROJDIR, null).loadConfig(function(err, cfg) {
+      assert.equal(err, null);
+      assert.deepEqual(Object.keys(cfg).sort(), TOP_LEVEL_KEYS);
+      assert.deepEqual(Object.keys(cfg.Boxen), ['AWSBox']);
+      assert.equal(cfg.Boxen.AWSBox.Type, 'AWSBox');
+      assert.deepEqual(Object.keys(cfg.Boxen.AWSBox.Properties).sort(),
+                       ['env', 'processes']);
+      assert.deepEqual(Object.keys(cfg.Resources).sort(),
+                       ["AWSBoxSecurityGroup","AWSBoxServer"]);
+      done();
+    });
+  });
+
+  it('merges profile AWSBox settings from top-level keys', function(done) {
+    projinfo(PROJDIR, 'ExtraAWSBoxSettingsTL').loadConfig(function(err, cfg) {
+      assert.equal(err, null);
+      assert.deepEqual(Object.keys(cfg).sort(), TOP_LEVEL_KEYS);
+      assert.deepEqual(Object.keys(cfg.Boxen), ['AWSBox']);
+      assert.deepEqual(Object.keys(cfg.Boxen.AWSBox.Properties).sort(),
+                       ['env', 'hooks', 'processes']);
+      assert.deepEqual(Object.keys(cfg.Resources).sort(),
+                       ["AWSBoxSecurityGroup","AWSBoxServer"]);
+      assert.deepEqual(cfg.Boxen.AWSBox.Properties.processes,
+                       ['somethingelse.js']);
+      done();
+    });
+  });
+
+  it('merges profile AWSBox settings from explicit decl', function(done) {
+    projinfo(PROJDIR, 'ExtraAWSBoxSettingsEX').loadConfig(function(err, cfg) {
+      assert.equal(err, null);
+      assert.deepEqual(Object.keys(cfg).sort(), TOP_LEVEL_KEYS);
+      assert.deepEqual(Object.keys(cfg.Boxen), ['AWSBox']);
+      assert.deepEqual(Object.keys(cfg.Boxen.AWSBox.Properties).sort(),
+                       ['env', 'hooks', 'processes']);
+      assert.deepEqual(Object.keys(cfg.Resources).sort(),
+                       ["AWSBoxSecurityGroup","AWSBoxServer"]);
+      assert.deepEqual(cfg.Boxen.AWSBox.Properties.processes,
+                       ['somethingelse.js']);
+      done();
+    });
+  });
+
+  it('allows a profile to remove the default AWSBox boxen', function(done) {
+    projinfo(PROJDIR, 'RemoveDefaultBoxen').loadConfig(function(err, cfg) {
+      assert.equal(err, null);
+      assert.deepEqual(Object.keys(cfg).sort(), TOP_LEVEL_KEYS);
+      assert.deepEqual(Object.keys(cfg.Boxen), []);
+      done();
+    });
+  });
+
+});
